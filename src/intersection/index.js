ce3aba588f38582fd86f76abc72154b757c0fb92
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-08-02 12:00:28 -0400

    updating intersection function

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,18 @@
-function intersection(a, b) {}
+function intersection (a, b) {
+  const numbers = new Map();
+  const intersectSet = new Set();
+
+  for (let num of a) {
+    numbers.set(num, 1);
+  }
+
+  for (let num of b) {
+    if (numbers.has(num)) {
+      intersectSet.add(num);
+    }
+  }
+  return [...intersectSet];
+  //another way to do this?
+}
 
 module.exports = intersection;
